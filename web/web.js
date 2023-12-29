79717681ca020194d1b4605158b4c79d37b3ceac
Alex <jk1143298@gmail.com>
2023-12-29 19:16:13 +0100

    update web.js

--- web/web.js
+++ web/web.js
@@ -31,18 +31,16 @@ function make_array(size) {
   }, { unit: 'MB' })
   console.log(memoryUsage2)
 
-  // console.log(performance.measureUserAgentSpecificMemory())
-  console.log(performance.memory)
-  make_array(10000)
-  console.log(performance.memory)
   /*
-  let memoryUsage3 = await trace_memory( () => {
-    make_array(100000)
+  console.log(performance.memory)
+  make_array(100000)
+  console.log(performance.memory)
+  */
+  memoryUsage = await trace_memory( () => {
+    make_array(1000000)
    // make_array(1)
   }, { unit: 'MB' })
-  */
-  //  console.log(memoryUsage3)
-  // console.log(performance.measureUserAgentSpecificMemory())
+  console.log(memoryUsage)
 
 })()
 /*
